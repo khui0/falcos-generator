17cb8050a24ce37fe3b7e7d4643a997f2075954d
Kenny <101839505+khui0@users.noreply.github.com>
2023-12-10 15:20:09 -0500

    Working CSV import and preview

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./reset.css";
 import "./style.css";
 import "./fonts.css";
+import "remixicon/fonts/remixicon.css";
 
 import { Falcos } from "./falcos.js";
 
@@ -17,9 +18,91 @@ const promises = [
     document.fonts.ready,
 ];
 
-falcos.initializeFont("16px Montserrat-SemiBold");
-falcos.initializeFont("16px LinLibertineCapitalsB");
+let ready = false;
+
+let data;
+let previewIndex = 0;
 
 Promise.all(promises).then(() => {
-    ctx.drawImage(falcos.generate("New Decade Resolutions Testing", ["Seancris Luyun", "Aidan Andres", "Hameed Abdul"]), 0, 0);
+    ready = true;
 });
+
+const fileInput = document.getElementById("file");
+fileInput.addEventListener("change", () => {
+    if (fileInput.files.length == 1) {
+        const file = fileInput.files[0];
+        file.text().then(result => {
+            data = parseCSV(result);
+            console.log(data);
+            update();
+        });
+    }
+});
+
+document.getElementById("left").addEventListener("click", () => {
+    previewIndex--;
+    update();
+});
+
+document.getElementById("right").addEventListener("click", () => {
+    previewIndex++;
+    update();
+});
+
+document.getElementById("preview-index").addEventListener("input", e => {
+    previewIndex = e.target.value;
+    update();
+});
+
+function update() {
+    previewIndex = clamp(previewIndex, 0, data.length - 1);
+    const story = data[previewIndex];
+    ctx.drawImage(falcos.generate(story.title, story.nominees), 0, 0);
+
+    document.getElementById("preview-index").value = previewIndex;
+    document.getElementById("preview-info").textContent = story.title;
+}
+
+function parseCSV(raw) {
+    try {
+        // Split CSV into two-dimensional array
+        let array = raw.split("\n");
+        array = array.map(row => row.split(","));
+        // Remove rows that do not have a story title
+        array = array.filter(row => row[columnToIndex("D")]?.trim());
+        // Remove the first row
+        array.shift();
+        // Isolate the story title and nominee names
+        let formatted = array.map(row => {
+            const nomineeOrder = "NOPQSTUVWXHIJKLF".split("").map(letter => columnToIndex(letter));
+            const nominees = [];
+            nomineeOrder.forEach(index => {
+                if (row[index]) {
+                    nominees.push(row[index].trim());
+                }
+            });
+            return {
+                title: row[columnToIndex("D")].trim(),
+                nominees: nominees,
+            };
+        });
+        return formatted;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function columnToIndex(column) {
+    const letters = column.split("").reverse();
+    let total = 0;
+    for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        const value = (letter.charCodeAt(0) - 65 + 1) * (26 ** i);
+        total += value;
+    }
+    return total - 1;
+}
+
+function clamp(num, min, max) {
+    return Math.min(Math.max(num, min), max);
+}
